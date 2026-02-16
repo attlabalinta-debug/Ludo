8a657052276a1d3ba7661067bae8595929ebb2fd
Bálint <attila.balint@evosoft.com>
2026-02-16 12:19:30 +0100

    Track Firebase web config for deployed client sync

---
+++ weekly-organizer/firebase-config.js
@@ -0,0 +1,9 @@
+window.WEEKLY_ORGANIZER_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC644dkk4KBlG0eny7zdxkWR64q9Rw9IPU",
+  authDomain: "ludotravel-77c06.firebaseapp.com",
+  databaseURL: "https://ludotravel-77c06-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ludotravel-77c06",
+  storageBucket: "ludotravel-77c06.firebasestorage.app",
+  messagingSenderId: "119770498174",
+  appId: "1:119770498174:web:70e9a43c74d8e70c4e442f",
+};
